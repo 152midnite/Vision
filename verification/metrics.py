8327284eb733a42de8c5a3fb38cbabf12085ee13
dante <p.k.golabek@gmail.com>
2019-08-27 12:03:19 +0200

    modified:   metrics.py - added hamming metric

--- verification/metrics.py
+++ verification/metrics.py
@@ -23,3 +23,33 @@ for element in answers:
             result += 1
 print(result)
 print(len(answers))
+
+
+def hamming_distance(s1, s2):
+    """Return the Hamming distance between equal-length sequences"""
+    if len(s1) != len(s2):
+        #print("Undefined for sequences of unequal length")
+        return None
+    return sum(el1 != el2 for el1, el2 in zip(s1, s2))
+
+distances = []
+for element in answers:
+    if len(answers[element]) == None:
+        pass
+    elif len(answers[element]) > 1:
+        sub_distances = []
+        for i in answers[element]:
+            sub_distances.append(hamming_distance(element,i))
+        if sub_distances:
+            sub_distances = [x for x in sub_distances if x is not None]
+            distances.append(min(sub_distances))
+    else:
+        distance = hamming_distance(element,answers[element][0])
+        distances.append(distance)
+
+distances = [x for x in distances if x is not None]
+print(distances)
+print(sum(distances)/len(distances))
+
+
+
